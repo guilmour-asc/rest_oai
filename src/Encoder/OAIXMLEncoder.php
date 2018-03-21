35624e586bbf17a7d0242913767205725e8142a2
Guilmour Alves da Silva Cunha <guilmour.asc@gmail.com>
2018-03-21 18:15:11 -0300

    First verb LISTRECORDS done; 5 verbs remaining. Still managing to insert namespaces on XML, as well as change rootName from <response> to <OAI>.

---
+++ src/Encoder/OAIXMLEncoder.php
@@ -0,0 +1,20 @@
+<?php
+// use \Drupal\serialization\Encoder\XmlEncoder;
+// use \Symfony\Component\Serializer\Encoder\EncoderInterface;
+
+// class OAIXMLEncoder extends XmlEncoder implements EncoderInterface{
+
+//   /**
+//    * {@inheritdoc}
+//    */
+//   static protected $format = ['oaixml'];
+
+//   /**
+//    * {@inheritdoc}
+//    */
+//   public function getBaseEncoder() {
+//     $base_encoder = parent::getBaseEncoder();
+//     $base_encoder->setRootNodeName('OAI');
+//     return $base_encoder;
+//   }
+// }
--- src/Plugin/rest/resource/OAI_Resource.php
+++ src/Plugin/rest/resource/OAI_Resource.php
@@ -13,15 +13,15 @@ use Psr\Log\LoggerInterface;
  * Provides a resource to get view modes by entity and bundle.
  *
  * @RestResource(
- *   id = "oai_list_records",
- *   label = @Translation("OAI: List Records"),
+ *   id = "oai_resource",
+ *   label = @Translation("OAI: Requests by Verb"),
  *   uri_paths = {
- *     "canonical" = "/oai/listrecords"
+ *     "canonical" = "/oai/{verb}"
  *   }
  * )
  */
 
-class OAI_ListRecords extends ResourceBase {
+class OAI_Resource extends ResourceBase {
   
   /**
    * A current user instance.
@@ -75,24 +75,55 @@ class OAI_ListRecords extends ResourceBase {
   /**
    * Responds to GET requests.
    *
-   * Returns a list of bundles for specified entity.
+   * Returns a list of bundles for specified entity, or a single node, or even the library's info.
+   * Response is dependent of the verb passed
    *
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    *   Throws exception expected.
    */
-  public function get() {
+  public function get($verb) {
     // You must implement the logic of your REST Resource here.
-    // for usage without need to authenticate the request, give permission for anonymous users' access...
-    // ... to the fucntion "Access GET on OAI-mimic Resource resource"...
-    // ... on the Permissions' Configuration Page.
+    // For usage without need to authenticate the request, give permission for anonymous users' access...
+    // ... to the fucntion "Access GET on OAI: Requests by Verb resource"...
+    // ... on section "RESTful Web Services"...
+    // ... on the PERMISSIONS' Configuration Page.
+    
     if (!$this->currentUser->hasPermission('access content')) {
       throw new AccessDeniedHttpException();
     }
-    $entities = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->loadByProperties(['type' => 'item']);
-    foreach ($entities as $entity) {
-      $result[$entity->id()] = array(
+
+    switch ($verb) {
+      case 'listrecords':
+        $response = listRecords();
+        return $response;
+        break;
+      
+      default:
+        # code...
+        break;
+    }
+  }
+}
+
+function listRecords(){
+  $entities = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadByProperties(['type' => 'item']);
+  $result['responseDate'] = date("l, Y-m-d, h:i:s A");
+  $result['request verb="ListRecords" set="?????"'] = 'http://solidaridadlibrary.org/oai/';
+  foreach ($entities as $entity) {
+    $result[$entity->id()] = array(
+      'header' => array(
+        'identifier' => '',
+        'datestamp' => '',
+        'setSpec' => ''
+      ),
+      'metadata' => array(
+        'dc:identifier' => array(
+          'dc:identifier:number' => $entity->id(),
+          'dc:identifier:uri' => \Drupal\rest_oai\Plugin\rest\resource\getFileURI($entity->get('field_digital_document')->value, $entity->get('field_document_upload'), $entity->get('field_document_url')),
+          'dc:identifier:citation' => $entity->get('field_citation')->value,
+        ),
         'dc:title' => $entity->title->value,
         'dc:creator' => $entity->get('field_creator_personal')->value,
         'dc:citation' => $entity->get('field_citation')->value,
@@ -100,7 +131,6 @@ class OAI_ListRecords extends ResourceBase {
         'dc:subject' => \Drupal\rest_oai\Plugin\rest\resource\getReferences($entity->get('field_subject')),
         'dc:coverage' => $entity->get('field_coverage')->value,
         'dc:description' => $entity->get('field_description')->value,
-        'dc:citation' => $entity->get('field_citation')->value,
         'dc:type' => $entity->get('field_type')->value,
         'dc:format' => $entity->get('field_format')->value,
         'dc:language scheme="ags:ISO639-2"' => $entity->get('field_language')->value,
@@ -110,16 +140,28 @@ class OAI_ListRecords extends ResourceBase {
         'dc:relation:ispartof' => \Drupal\rest_oai\Plugin\rest\resource\getReferences($entity->get('field_related_document')),
         'dc:source' => $entity->get('field_source')->value,
         'dc:rights' => $entity->get('field_rights')->value,
-        'dc:identifier:uri' => \Drupal\rest_oai\Plugin\rest\resource\getFileURI($entity->get('field_digital_document')->value, $entity->get('field_document_upload'), $entity->get('field_document_url')),
         'dcterms:dateIssued' => $entity->get('field_date_issued')->value,
-        'bitch' => array('tsu:gero' => 'ma pussy pops'),
-        );
-    }
-    unset($entity);
-    $response = new ResourceResponse($result);
-    $response->addCacheableDependency($result);
-    return $response;
+        'bitch' => array('tsu:gero' => 'ma pussy pops')
+        )
+      );
   }
+  unset($entity);
+  $response = new ResourceResponse($result);
+  $response->addCacheableDependency($result);
+  return $response;
+}
+
+function listSets(){
+  $entities = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadByProperties(['type' => 'collection']);
+  foreach ($entities as $entity) {
+
+  }
+  unset($entity);
+  $response = new ResourceResponse($result);
+  $response->addCacheableDependency($result);
+  return $response;
 }
 
 // Getting the listage of references (subject, related references/document, etc.), of the item...
